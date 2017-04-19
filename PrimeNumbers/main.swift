eb5050ad8473b8d0439d18d3099b33b5e4316eef
Lennart Wisbar <anlennart@gmail.com>
2017-04-19 16:15:52 +0200

    Initial Commit

---
+++ PrimeNumbers/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PrimeNumbers
+//
+//  Created by Lennart Wisbar on 19.04.17.
+//  Copyright © 2017 Lennart Wisbar. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
