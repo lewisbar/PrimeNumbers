b65e7f1704f1bf78d7bf1015e34e08842ecba21c
Lennart Wisbar <anlennart@gmail.com>
2017-04-19 16:49:57 +0200

    Moved the Int extension to a separate file.
    The extension is the reusable part. You could just drop the file in a
    project and use it.

---
+++ PrimeNumbers/Int+Extension.swift
@@ -0,0 +1,34 @@
+//
+//  Int+Extension.swift
+//  PrimeNumbers
+//
+//  Created by Lennart Wisbar on 19.04.17.
+//  Copyright © 2017 Lennart Wisbar. All rights reserved.
+//
+
+import Foundation
+
+extension Int {
+    var isAPrimeNumber: Bool {
+        guard self > 3 else {
+            return true
+        }
+        
+        for divisor in 2...(self/2) {
+            if (self % divisor) == 0 {
+                return false
+            }
+        }
+        return true
+    }
+    
+    static func primeNumbers(from firstNumber: Int = 1, through lastNumber: Int) -> [Int] {
+        var primeNumbers: [Int] = []
+        for number in firstNumber...lastNumber {
+            if number.isAPrimeNumber {
+                primeNumbers.append(number)
+            }
+        }
+        return primeNumbers
+    }
+}
--- PrimeNumbers/main.swift
+++ PrimeNumbers/main.swift
@@ -8,32 +8,6 @@
 
 import Foundation
 
-extension Int {
-    var isAPrimeNumber: Bool {
-        guard self > 3 else {
-            return true
-        }
-        
-        for divisor in 2...(self/2) {
-            if (self % divisor) == 0 {
-                return false
-            }
-        }
-        return true
-    }
-    
-    static func primeNumbers(from firstNumber: Int = 1, through lastNumber: Int) -> [Int] {
-        var primeNumbers: [Int] = []
-        for number in firstNumber...lastNumber {
-            if number.isAPrimeNumber {
-                primeNumbers.append(number)
-            }
-        }
-        return primeNumbers
-    }
-}
-
-
 print("I will calculate all prime numbers from the first through the second number you enter.")
 print("Enter first number:")
 let firstNumber = Int(readLine()!)!
