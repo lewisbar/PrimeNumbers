7a3acdd9bde70e80e7678fd0cb6496bb241aba72
Lennart Wisbar <lewisbar.git@gmail.com>
2017-05-05 10:56:06 +0200

    Optimized calculation. Designed a safe command line interaction.

--- PrimeNumbers/Int+PrimeNumbers.swift
+++ PrimeNumbers/Int+PrimeNumbers.swift
@@ -12,17 +12,22 @@ extension Int {
     var isAPrimeNumber: Bool {
         guard self > 1 else { return false }
         guard self > 3 else { return true }
+        guard (self % 2) != 0 else { return false }
         
-        for divisor in 2...(self/2) {
+        var divisor = 3
+        let upperBound = Int(sqrt(Double(self)))
+        while divisor <= upperBound {
             if (self % divisor) == 0 {
                 return false
             }
+            divisor += 2
         }
         return true
     }
     
-    static func primeNumbers(from firstNumber: Int = 1, through lastNumber: Int) -> [Int] {
+    static func primeNumbers(from firstNumber: Int, through lastNumber: Int) -> [Int] {
         var primeNumbers: [Int] = []
+        if firstNumber <= 2 { return Int.primeNumbers(through: lastNumber) } // Use optimized function
         for number in firstNumber...lastNumber {
             if number.isAPrimeNumber {
                 primeNumbers.append(number)
@@ -30,4 +35,71 @@ extension Int {
         }
         return primeNumbers
     }
+    
+    static func primeNumbers(through lastNumber: Int) -> [Int] {
+        var primeNumbers = [2]
+        
+        func isAPrimeNumber(_ number: Int) -> Bool {
+            let upperBound = Int(Float(number).squareRoot())
+            for divisor in primeNumbers {
+                if (number % divisor) == 0 {
+                    return false
+                }
+                if divisor >= upperBound {
+                    return true
+                }
+            }
+            return true
+        }
+        
+        var number = 3
+        while number <= lastNumber {
+            if isAPrimeNumber(number) {
+                primeNumbers.append(number)
+            }
+            number += 2
+        }
+        return primeNumbers
+    }
+    
+//    static func primeNumbersWithOptimizedPerformance(lastNumber: Int) -> [Int] {
+//        var primeNumbers: [Int] = [2]
+//        
+//        for number in 3...lastNumber {
+//            if (number % divisor) == 0 {
+//                break
+//            }
+//        }
+//        
+//        return primeNumbers
+//    }
+
+//    static func primeNumbersOptimized(through lastNumber: Int) -> [Int] {
+//        var primeNumbers: [Int] = [2]
+//        var testNumber = 3
+//        var couldBePrime = true
+//        var index = 0
+//        
+//        while testNumber <= lastNumber {
+//            while couldBePrime == true {
+//                if testNumber % primeNumbers[index] == 0 {
+//                    couldBePrime = false
+//                    testNumber += 2
+//                    index = 0
+//                } else {
+//                    if primeNumbers[index] >= testNumber / (testNumber+2){ //primeNumbers[index+1]{
+//                        //primeNumbers.append(testNumber)
+//                        print(testNumber)
+//                        testNumber += 2
+//                        index = 0
+//                    } else {
+//                        index += 1
+//                    }
+//                    couldBePrime = true
+//                }
+//            }
+//        }
+//        return primeNumbers
+//    }
+
 }
--- PrimeNumbers/main.swift
+++ PrimeNumbers/main.swift
@@ -8,22 +8,44 @@
 
 import Foundation
 
-print("I will calculate all prime numbers from the first through the second number you enter.")
-print("Enter first number:")
-
-let firstNumber = Int(readLine()!)!
-
-print("\nEnter last number:")
-
-let lastNumber = Int(readLine()!)!
-let primeNumbersResult = Int.primeNumbers(from: firstNumber, through: lastNumber)
-
-print("\nThese are all prime numbers from \(firstNumber) through \(lastNumber):")
-
-for number in primeNumbersResult {
-    print(number)
+let ending = "\n\n=================================\n\n"
+var number = 1
+while number > 0 {
+    print("Enter a number up to which you want to know all prime numbers (0 to quit):")
+    guard let line = readLine(),
+        let lastNumber = Int(line),
+        lastNumber >= 0 else { print("Invalid input." + ending); continue }
+    number = lastNumber
+    guard number > 0 else { print("Goodbye!" + ending); continue }
+    print("Calculating ...")
+    let primeNumbers = Int.primeNumbers(through: number)
+    guard let lastPrimeNumber = primeNumbers.last else { print("No result." + ending); continue }
+    for number in primeNumbers {
+        var terminator = ", "
+        if number == lastPrimeNumber {
+            terminator = "." + ending
+        }
+        print(number, terminator: terminator)
+    }
 }
 
-for _ in 1...2 {
-    print("=============")
-}
+
+//print("I will calculate all prime numbers from the first through the second number you enter.")
+//print("Enter first number:")
+//
+//let firstNumber = Int(readLine()!)!
+//
+//print("\nEnter last number:")
+//
+//let lastNumber = Int(readLine()!)!
+//let primeNumbersResult = Int.primeNumbers(from: firstNumber, through: lastNumber)
+//
+//print("\nThese are all prime numbers from \(firstNumber) through \(lastNumber):")
+//
+//for number in primeNumbersResult {
+//    print(number)
+//}
+//
+//for _ in 1...2 {
+//    print("=============")
+//}
